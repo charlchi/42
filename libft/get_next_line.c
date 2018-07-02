060bbc20a89849a57ab5e0db96ec26592e91c82c
Charl MOLLER <cmoller@c2r3s6.wethinkcode.co.za>
2018-07-02 12:52:58 +0200

    compile libft with a Make -C ../libft instead

--- libft/get_next_line.c
+++ libft/get_next_line.c
@@ -6,19 +6,11 @@
 /*   By: cmoller <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/23 12:51:45 by cmoller           #+#    #+#             */
-/*   Updated: 2018/05/31 10:19:35 by cmoller          ###   ########.fr       */
+/*   Updated: 2018/06/18 15:05:05 by cmoller          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-int		gnl_strcpy(char *src, char *dst)
-{
-	while (*src)
-		*dst++ = *src++;
-	*dst = 0;
-	return (0);
-}
 
 int		gnl_strlen(char *src)
 {
@@ -30,48 +22,74 @@ int		gnl_strlen(char *src)
 	return (i);
 }
 
-int		get_next_line(const int fd, char **l)
+int		gnl_strccpy(char *src, char *dst, int c)
 {
-	static char		buff[1000][BUFF_SIZE + 1];
+	int		i;
+
+	i = 0;
+	while (src[i])
+	{
+		dst[i] = src[i];
+		if (dst[i] == (char)c)
+		{
+			dst[i] = 0;
+			return (i);
+		}
+		i++;
+	}
+	dst[i] = 0;
+	return (i);
+}
+
+int		gnl_getfdbuff(char **b, const int fd)
+{
+	static t_fd		*fb[5000];
 	int				i;
-	int				j;
 	int				r;
 
-
-
-	if ((!l || f < 0))
+	i = -1;
+	while (fb[++i] != NULL)
+		if (fb[i]->fd == fd)
+			break ;
+	if (fb[i] == NULL)
+	{
+		if (!(fb[i] = (t_fd *)malloc(sizeof(t_fd))))
+			return (-1);
+		fb[i]->fd = fd;
+		fb[i]->b[0] = 0;
+	}
+	*b = fb[i]->b;
+	if (read(fd, 0, 0) != 0)
 		return (-1);
+	if ((r = gnl_strlen(&(*b)[0])) == 0)
+		if ((r = read(fd, &(*b)[0], BUFF_SIZE)) >= 0)
+			(*b)[r] = 0;
+	return (r);
+}
 
-	if (!(*l = (char *)malloc(52000))) // TODO: add gnl_realloc();
+int		get_next_line(const int fd, char **l)
+{
+	char			*b;
+	int				j;
+	int				i;
+	int				r;
+
+	if (l == NULL || fd < 0 || BUFF_SIZE < 1)
 		return (-1);
-
-	// if buff isn't empty, get its strlen as r
-	// you still have characters to process from previous line
-	r = gnl_strlen(&buff[f][0]);
-	if (r == 0) // if there isn't any characters in buff, read()
-		r = read(f, &buff[f][0], BUFF_SIZE);
-	b[f][r] = 0; // add '\0' to buffer. buff is [BUFFSIZ + 1] for this
-	i = 0;
+	if (!(*l = (char *)malloc(5000000)))
+		return (-1);
+	r = gnl_getfdbuff(&b, fd);
+	(*l)[0] = 0;
+	j = 0;
 	while (r > 0)
 	{
-		j = 0;
-		while (j < r) // copy over characters from buffer one by one
-		{
-			// TODO	if ( i + r > current line malloc size)
-			// 			gnl_realloc())
-			(*l)[i++] = b[f][j++];
-			if ((*l)[i - 1] == '\n') // if newline
-				// push back characters in buffer and return;
-			{
-				gnl_strcpy(&b[f][j], &b[f][0]);
-				(*l)[i - 1] = 0;
-				return (1);
-			}
-		}
-		r = read(f, &b[f][0], BUFF_SIZE);
-		b[f][r] = 0;
+		i = gnl_strlen(*l);
+		if (b[(j = gnl_strccpy(b, &(*l)[i], '\n'))] == '\n')
+			return (gnl_strccpy(&b[j + 1], &b[0], 0) + 1 > 0);
+		if ((r = read(fd, &b[0], BUFF_SIZE)) >= 0)
+			b[r] = 0;
 	}
-	if (i > 0) // r == 0 : EOF encountered, but line is not null
+	if (j > 0)
 		return (1);
 	return (r);
 }
