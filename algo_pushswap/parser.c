193cb41401706ba239f9a5cd8664bbd6c9ed37bd
Charl MOLLER <cmoller@c1r3s1.wethinkcode.co.za>
2018-07-31 14:22:30 +0200

    push_swap fixed completely

--- algo_pushswap/parser.c
+++ algo_pushswap/parser.c
@@ -6,7 +6,7 @@
 /*   By: cmoller <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/09 09:10:11 by cmoller           #+#    #+#             */
-/*   Updated: 2018/07/09 09:10:13 by cmoller          ###   ########.fr       */
+/*   Updated: 2018/07/31 14:21:53 by cmoller          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,23 @@ void	parse_args(t_env *env)
 {
 	int			i;
 
-	if (env->argc < 2)
-		exit_ps();
+	env->argv++;
+	env->argc--;
+	if (env->argc == 1)
+	{
+		env->argv = ft_strsplit(env->argv[0], ' ');
+		env->argc = ft_strstrlen(env->argv);
+	}
 	if (!valid_nums(env) || !valid_dups(env))
 		exit_ps();
 	if (!(env->a = (t_stack *)malloc(sizeof(t_stack))))
 		exit_ps();
 	if (!(env->b = (t_stack *)malloc(sizeof(t_stack))))
 		exit_ps();
-	init_stack(env->a, env->argc - 1);
-	init_stack(env->b, env->argc - 1);
+	init_stack(env->a, env->argc + 1);
+	init_stack(env->b, env->argc + 1);
 	i = env->argc - 1;
-	while (i > 0)
+	while (i >= 0)
 	{
 		st_push(env->a, ft_atoi(env->argv[i]));
 		i--;
@@ -40,7 +45,7 @@ int		valid_nums(t_env *env)
 	int		j;
 	char	c;
 
-	i = 1;
+	i = 0;
 	while (i < env->argc)
 	{
 		j = 0;
@@ -65,7 +70,7 @@ int		valid_dups(t_env *env)
 	int		i;
 	int		j;
 
-	i = 1;
+	i = 0;
 	while (i < env->argc - 1)
 	{
 		j = i + 1;
