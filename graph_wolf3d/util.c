d380afe8e3ab1855f8da83377cf997beb513c252
Charl MOLLER <cmoller@c1r2s6.wethinkcode.co.za>
2018-08-09 14:00:52 +0200

    wolf3d normed

--- graph_wolf3d/util.c
+++ graph_wolf3d/util.c
@@ -6,7 +6,7 @@
 /*   By: cmoller <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/17 16:45:05 by cmoller           #+#    #+#             */
-/*   Updated: 2018/07/17 16:45:07 by cmoller          ###   ########.fr       */
+/*   Updated: 2018/08/09 13:22:01 by cmoller          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 float		get_ray_dist(t_cam *cam)
 {
-	float 				x;
-	float 				y;
+	float				x;
+	float				y;
 
 	x = cam->rx;
 	y = cam->ry;
@@ -34,27 +34,59 @@ void		handle_input(t_env *env)
 {
 	float				oldx;
 	float				oldy;
-	long				micro;
 
 	oldx = env->x;
 	oldy = env->y;
-	micro = get_micro_time();
-	env->dt = (micro - env->pt) / 1000.0;
-	env->pt = micro;
 	if (env->keys[XK_w] || env->keys[XK_s])
 	{
-		env->x += 0.004 * cos(env->rot) * env->dt * (env->keys[XK_s] ? -1 : 1);
-		env->y += 0.004 * sin(env->rot) * env->dt * (env->keys[XK_s] ? -1 : 1);
+		env->x += 0.004 * cos(env->rot) *
+			env->dt * (env->keys[XK_s] ? -1 : 1);
+		env->y += 0.004 * sin(env->rot) *
+			env->dt * (env->keys[XK_s] ? -1 : 1);
 	}
 	if (env->keys[XK_d] || env->keys[XK_a])
 	{
-		env->x += 0.004 * cos(env->rot + 1.7) * env->dt * (env->keys[XK_a] ? -1 : 1);
-		env->y += 0.004 * sin(env->rot + 1.7) * env->dt * (env->keys[XK_a] ? -1 : 1);
+		env->x += 0.004 * cos(env->rot + 1.7) *
+			env->dt * (env->keys[XK_a] ? -1 : 1);
+		env->y += 0.004 * sin(env->rot + 1.7) *
+			env->dt * (env->keys[XK_a] ? -1 : 1);
 	}
 	env->x = env->map[(int)oldy][(int)env->x] > 0 ? oldx : env->x;
 	env->y = env->map[(int)env->y][(int)oldx] > 0 ? oldy : env->y;
 	env->rot -= ((env->keys[XK_Left]) ? 0.004 : 0.0) * env->dt;
-	env->rot += ((env->keys[XK_Right]) ? 0.004: 0.0) * env->dt;
-	env->focal -= env->keys[XK_r] ? 0.01 : 0.0;
-	env->focal += env->keys[XK_f] ? 0.01 : 0.0;
+	env->rot += ((env->keys[XK_Right]) ? 0.004 : 0.0) * env->dt;
+	env->focal -= env->keys[XK_r] ? 0.1 / env->dt : 0.0;
+	env->focal += env->keys[XK_f] ? 0.1 / env->dt : 0.0;
+}
+
+void		load_images(t_env *env, char *walls, char *ceil)
+{
+	env->tex = (int **)malloc(640 * sizeof(int *));
+	env->ftex = (int **)malloc(640 * sizeof(int *));
+	load_image(walls, &env->tex);
+	load_image(ceil, &env->ftex);
+}
+
+void		load_image(char *file, int ***tex)
+{
+	int		i;
+	int		j;
+	int		k;
+	void	*img;
+	int		*data;
+
+	img = mlx_xpm_file_to_image(get_mlx(), file, &i, &j);
+	data = (int *)(mlx_get_data_addr(img, &i, &j, &k));
+	i = 0;
+	while (i < 640)
+	{
+		(*tex)[i] = (int *)malloc(640 * sizeof(int));
+		j = 0;
+		while (j < 640)
+		{
+			(*tex)[i][j] = data[j + i * 640];
+			j++;
+		}
+		i++;
+	}
 }
--- graph_wolf3d/wolf3d.c
+++ graph_wolf3d/wolf3d.c
@@ -6,29 +6,26 @@
 /*   By: cmoller <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/17 16:44:51 by cmoller           #+#    #+#             */
-/*   Updated: 2018/07/31 11:42:12 by cmoller          ###   ########.fr       */
+/*   Updated: 2018/08/09 13:18:22 by cmoller          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "wolf3d.h"
 
-
-int texwidth = 640;
-int tex[640][640];
-int ftex[640][640];
-
-void	setup_cam(t_env *env, t_cam *cam, int *img, int x)
+void	setup_cam(t_env *env, t_cam *cam, int x)
 {
 	cam->sweep = (float)x / (float)env->h * 1.0 - 0.5;
 	cam->angle = atan(cam->sweep / env->focal);
 	cam->rx = cos(env->rot + cam->angle);
 	cam->ry = sin(env->rot + cam->angle);
 	cam->step = 0.01;
-	while (!env->map[(int)(env->y + cam->step * cam->ry)][(int)(env->x + cam->step * cam->rx)])
+	while (!env->map[(int)(env->y + cam->step * cam->ry)]
+			[(int)(env->x + cam->step * cam->rx)])
 		cam->step += 0.01;
 	cam->hitx = (int)(env->x + cam->step * cam->rx);
 	cam->hity = (int)(env->y + cam->step * cam->ry);
-	cam->side = cam->hity != (int)(env->y + (cam->step - 0.01) * cam->ry) ? 0 : 1;
+	cam->side =
+		cam->hity != (int)(env->y + (cam->step - 0.01) * cam->ry) ? 0 : 1;
 	cam->rx = cam->rx * cam->step;
 	cam->ry = cam->ry * cam->step;
 	cam->h = 0.5 * env->focal * env->h / get_ray_dist(cam);
@@ -42,15 +39,16 @@ void	draw_walls(t_env *env, t_cam *cam, int *img, int x)
 	int		xtex;
 	int		ytex;
 	float	sidescale;
-	float	temp;
 
 	y = cam->ds;
 	while (y <= cam->de)
 	{
-		ytex = abs((int)((y + cam->h - env->h / 2) / cam->h * texwidth) / 2);
-		sidescale = cam->side ? cam->hity - (env->y + cam->ry) : cam->hitx - (env->x + cam->rx);
-		xtex = abs((int)(sidescale * texwidth));
-		img[x + y * env->w] = tex[ytex][xtex];
+		ytex = abs((int)((y + cam->h - env->h / 2) / cam->h * 640) / 2);
+		sidescale =
+			cam->side ? cam->hity - (env->y + cam->ry) :
+			cam->hitx - (env->x + cam->rx);
+		xtex = abs((int)(sidescale * 640));
+		img[x + y * env->w] = env->tex[ytex][xtex];
 		y++;
 	}
 }
@@ -60,29 +58,28 @@ void	draw_floor(t_env *env, t_cam *cam, int *img, int x)
 	int		y;
 	int		ytex;
 	int		xtex;
-	float	temp;
 
 	y = cam->de + 1;
 	while (y < env->h - 1)
 	{
-		cam->step =  env->h / (2.0 * (y+0) - env->h) / get_ray_dist(cam) * env->focal;
+		cam->step = env->h / (2.0 * (y + 0) - env->h) /
+			get_ray_dist(cam) * env->focal;
 		ytex = (int)(fabs((env->y + cam->step * cam->ry) -
-			(int)(env->y + cam->step * cam->ry)) * texwidth);
+			(int)(env->y + cam->step * cam->ry)) * 640);
 		xtex = (int)(fabs((env->x + cam->step * cam->rx) -
-			(int)(env->x + cam->step * cam->rx)) * texwidth);
-		img[x + (env->h-y) * env->w] = ((int)(x * env->x) & (int)(y * env->y));
-		img[x + y          * env->w] = ((int)(x * env->x) & (int)(y * env->y));
+			(int)(env->x + cam->step * cam->rx)) * 640);
+		img[x + (env->h - y) * env->w] = env->ftex[ytex][xtex];
+		img[x + y * env->w] = env->ftex[ytex][xtex];
 		y++;
 	}
 }
-		
+
 int		draw(void *p)
 {
 	t_env			*env;
 	int				*img;
 	t_cam			*cam;
 	int				x;
-	int				y;
 
 	env = (t_env *)p;
 	cam = env->cam;
@@ -91,7 +88,7 @@ int		draw(void *p)
 	x = 0;
 	while (x < env->w)
 	{
-		setup_cam(env, cam, img, x);
+		setup_cam(env, cam, x);
 		draw_walls(env, cam, img, x);
 		draw_floor(env, cam, img, x);
 		x++;
@@ -100,53 +97,26 @@ int		draw(void *p)
 	return (0);
 }
 
-int		main(int argc, char **argv)
+int		main(void)
 {
-	int i1,i2,i3;
-	int widthw;
-	int heighth;
-	void	*wallim;
-	int		*wallimdata;
-	void	*floorim;
-	int		*floorimdata;
-	//xpmimage = NULL;
-	wallim = mlx_xpm_file_to_image(get_mlx(), "test.xpm", &widthw, &heighth);
-	wallimdata = (int *)(mlx_get_data_addr(wallim, &i1, &i2, &i3));
-	floorim = mlx_xpm_file_to_image(get_mlx(), "floor.xpm", &widthw, &heighth);
-	floorimdata = (int *)(mlx_get_data_addr(floorim, &i1, &i2, &i3));
-	int j;
-	int i = 0;
-	while (i < texwidth)
-	{
-		j = 0;
-		while (j < texwidth)
-		{
-			tex[i][j] = wallimdata[j + i * texwidth];
-			ftex[i][j] = floorimdata[j + i * texwidth];
-			j++;
-		}
-		i++;
-	}
-
 	t_env		*env;
 	t_cam		*cam;
 
 	env = malloc(sizeof(t_env));
 	cam = malloc(sizeof(t_cam));
 	env->cam = cam;
-	parse_args(env, argc, argv);
+	parse_args(env);
+	load_images(env, "test.xpm", "floor.xpm");
 	env->w = 1000;
 	env->h = 1000;
 	env->x = 5.0;
 	env->y = 5.0;
-	env->rot = 0.0;
 	env->focal = 0.7;
 	env->pt = get_micro_time();
-	if (!(env->mlx = get_mlx()) || !(env->win = get_win(env->w, env->h, "wolf3d")))
+	if (!(env->mlx = get_mlx()))
+		exit_error("get_mlx()");
+	if (!(env->win = get_win(env->w, env->h, "wolf3d")))
 		exit_error("get_win()");
-	//clear_img(get_img(&env->img, env->w, env->h), env->w, env->h, 0x0000000);
-	mlx_mouse_hook(env->win, mouse_hook, env);
-	mlx_hook(env->win, MotionNotify, PointerMotionMask, mouse_move_hook, env);
 	mlx_hook(env->win, 2, 1L << 0, key_down_hook, env);
 	mlx_key_hook(env->win, key_up_hook, env);
 	mlx_loop_hook(get_mlx(), draw_loop, env);
