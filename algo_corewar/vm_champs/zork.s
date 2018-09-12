82798b4600dd38bd5037f182af63dd17a7ffd10f
charlchi <cmoller@student.wethinkcode.co.za>
2018-09-12 15:51:15 +0200

    corewar

---
+++ algo_corewar/vm_champs/zork.s
@@ -0,0 +1,19 @@
+.name "zork"
+.comment "I'M ALIIIIVE"
+
+l1:   zjmp %:l9
+l2:   zjmp %:l9
+l3:   zjmp %:l9
+l4:   zjmp %:l9
+l5:   zjmp %:l9
+l6:   zjmp %:l9
+l7:   zjmp %:l9
+l8:   zjmp %:l9
+l9:   zjmp %:l9
+l10:   zjmp %:l1
+l11:   zjmp %:l1
+l12:   zjmp %:l1
+l13:   zjmp %:l1
+l14:   zjmp %:l1
+l15:   zjmp %:l1
+l16:   zjmp %:l1
